180eaa071261c5408f3d3ccfabd9fda01d0fd366
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2024-07-02 22:00:04 +0200

    working on the hero and navigation section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        figtree: ["Figtree", "sans-serif"],
+      },
+      backgroundImage: {
+        "custom-bg": "url(./assets/images/header-bg1.png)",
+      },
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
   plugins: [],
 };
