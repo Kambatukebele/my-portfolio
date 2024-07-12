fc6cffd2d6aca7803af59ae989235c8db8bdf4e1
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2024-07-12 18:56:57 +0200

    updated Services components

--- src/components/services/DownDiv.jsx
+++ src/components/services/DownDiv.jsx
@@ -1,13 +1,23 @@
 import CardServices from "../../components/services/CardServices";
 import { services } from "../../my_data";
+import Button from "../Button";
 const DownDiv = () => {
   console.log(services);
   return (
-    <div className="grid gap-4 grid-cols-1 grid-rows-1 sm:grid-cols-2 sm:grid-rows-2 md:grid-cols-3 lg:gap-10">
-      {services.map((service) => {
-        return <CardServices {...service} />;
-      })}
-    </div>
+    <>
+      <div className="grid gap-4 grid-cols-1 grid-rows-1 sm:grid-cols-2 sm:grid-rows-2 md:grid-cols-3 lg:gap-10">
+        {services.map((service) => {
+          return <CardServices {...service} />;
+        })}
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <Button
+          buttonText="Book a Call With me"
+          buttonExtraClass="bg-purple900 text-white"
+          buttonIconClass="bg-white text-blue"
+        />
+      </div>
+    </>
   );
 };
 export default DownDiv;
