2409eed83678ca0864e42db5da3f463e37b7086d
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2024-07-13 18:27:55 +0200

    updated CardServices.jsx

--- src/components/services/CardServices.jsx
+++ src/components/services/CardServices.jsx
@@ -2,7 +2,7 @@ import H4 from "../H4";
 import Paragraph from "../Paragraph";
 const CardServices = ({ id, icon, title, description }) => {
   return (
-    <div className="w-full flex flex-col justify-start items-start gap-4 shadow-md py-4 px-4 rounded-2xl bg-purple900 h-[300px] cursor-pointer duration-200 ease-in-out hover:bg-blue">
+    <div className="w-full flex flex-col justify-start items-start gap-4 shadow-md py-4 px-4 rounded-2xl bg-purple900 h-[300px] cursor-pointer duration-200 ease-in-out hover:-mt-5 hover:bg-blue">
       <span className="w-16 h-16 rounded-full flex justify-center items-center bg-white text-blue text-2xl">
         {icon}
       </span>
