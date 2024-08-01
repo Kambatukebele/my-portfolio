7495013b89dd4a6ed31726243d39c891ac4aa955
kamba tukebele <kambatukebele@gmail.com>
2024-08-01 08:40:40 +0200

    updated contact section by removing the form

--- src/components/contact/ContactInfo.jsx
+++ src/components/contact/ContactInfo.jsx
@@ -8,63 +8,63 @@ import Paragraph from "../Paragraph";
 import H4 from "../H4";
 const ContactInfo = () => {
   return (
-    <div className="hidden lg:flex lg:justify-center lg:flex-col lg:items-center lg:gap-4 lg:p-10 lg:w-2/5 lg:bg-black900 lg:rounded-2xl">
-      <div className="lg:flex lg:flex-col lg:justify-center lg:items-start lg:gap-4">
+    <div className="flex justify-center flex-col items-center gap-4 p-10 w-full bg-black900 rounded-2xl">
+      <div className="flex flex-col justify-center items-center gap-4">
         <H4
           h4Title="Contact Info"
-          h4ExtraClass="lg:text-white lg:font-semibold"
+          h4ExtraClass="text-white font-semibold"
         />
         <Paragraph
           paraText="If you're in need of a freelance Shopify developer, want a full Shopify ecommerce website, custom theme development or just need to hear some straightforward advice on how to start your next Shopify project, I'm all ears: let's have a chat!"
-          width="text-white lg:text-white"
+          width="text-white text-center lg:w-[700px]"
         />
       </div>
-      <div className="lg:w-full lg:flex lg:flex-col lg:justify-center lg:items-start lg:gap-4">
-        <div className="lg:flex lg:flex-col lg:justify-center lg:items-start lg:gap-2 lg:py-2">
-          <span className="lg:flex lg:justify-center lg:items-center lg:w-9 lg:h-9 lg:bg-purple50 lg:rounded-full">
+      <div className="w-full flex flex-col justify-center items-center gap-4">
+        <div className="w-full flex flex-col justify-center items-center gap-2 py-2">
+          <span className="flex justify-center items-center w-9 h-9 bg-purple50 rounded-full">
             <FontAwesomeIcon
               className="text-black900 text-2xl"
               icon={faLocation}
             />
           </span>
           <div>
-            <span className="lg:text-white lg:block lg:font-medium lg:text-xl lg:mb-1">
+            <span className="text-white block font-medium text-xl mb-1 text-center">
               Our Location
             </span>
             <Paragraph
               paraText="Remote / Based in Europe, Czech Republic"
-              width="lg:text-white"
+              width="text-white text-center"
             />
           </div>
         </div>
-        <div className="lg:flex lg:flex-col lg:justify-center lg:items-start lg:gap-2 lg:py-2">
-          <span className="lg:flex lg:justify-center lg:items-center lg:w-9 lg:h-9 lg:bg-purple50 lg:rounded-full">
+        <div className="w-full flex flex-col justify-center items-center gap-2 py-2">
+          <span className="flex justify-center items-center w-9 h-9 bg-purple50 rounded-full">
             <FontAwesomeIcon
               className="text-black900 text-2xl"
               icon={faPhone}
             />
           </span>
           <div>
-            <span className="lg:text-white lg:block lg:font-medium lg:text-xl lg:mb-1">
+            <span className="text-white block font-medium text-xl mb-1 text-center">
               Call or Whatsapp me
             </span>
-            <Paragraph paraText="+420 607 284 825" width="lg:text-white" />
+            <Paragraph paraText="+420 607 284 825" width="text-white text-center" />
           </div>
         </div>
-        <div className="lg:flex lg:flex-col lg:justify-center lg:items-start lg:gap-2 lg:py-2">
-          <span className="lg:flex lg:justify-center lg:items-center lg:w-9 lg:h-9 lg:bg-purple50 lg:rounded-full">
+        <div className="w-full flex flex-col justify-center items-center gap-2 py-2">
+          <span className="flex justify-center items-center w-9 h-9 bg-purple50 rounded-full">
             <FontAwesomeIcon
               className="text-black900 text-2xl"
               icon={faEnvelope}
             />
           </span>
           <div>
-            <span className="lg:text-white lg:block lg:font-medium lg:text-xl lg:mb-1">
+            <span className="text-white block font-medium text-xl mb-1 text-center">
               Email me
             </span>
             <Paragraph
-              paraText="kambatukebele@gmail.com"
-              width="lg:text-white"
+              paraText="kambatukebele@gmail.com, kamba@kambatukebele.com"
+              width="text-white text-center"
             />
           </div>
         </div>
--- src/components/contact/DownContact.jsx
+++ src/components/contact/DownContact.jsx
@@ -5,7 +5,7 @@ const DownContact = () => {
   return (
     <div className="w-full h-fit lg:flex lg:justify-center lg:items-start gap-4">
       <ContactInfo />
-      <ContactForm />
+      {/* <ContactForm /> */}
     </div>
   );
 };
